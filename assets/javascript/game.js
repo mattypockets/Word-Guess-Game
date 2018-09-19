68bbb6e0f7c73e29576b37f831e15901a5c99382
Matt Gasper <mrgasper27@gmail.com>
2018-09-19 13:49:56 -0500

    Built basic frame for HTML and CSS. Started array in JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+// List of words to pull from
+var wordBank = ["mario",
+"yoshi",
+"pikachu",
+"kirby",
+"donkey kong",
+"captain falcon",
+"fox",
+"link",
+"jigglypuff",
+"luigi",
+"samus",
+"ness"];
+
+// Choose a random word from wordBank
+var word = wordBank[Math.floor (Math.random()*wordBank.length]
+
+// Display _ for each letter in the random word
+
+// Check if input equals one of the letters
+
+// If input matches letter, replace _ with correct letter
+
+// If input does not match letter, reduce number of guesses by one
+
+// List which letters have been guessed
+
