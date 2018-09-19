9d2dfdc3cd69358bec3e8f2c44a9a23ff8978381
Matt Gasper <mrgasper27@gmail.com>
2018-09-19 18:58:33 -0500

    Added remaining HTML and some JS tweaks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,12 +12,26 @@ var wordBank = ["mario",
 "samus",
 "ness"];
 
+// Establish other base variables
+
+var wins = 0;
+var guessesLeft = 7;
+
+var letters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
+
 // Choose a random word from wordBank
-var word = wordBank[Math.floor (Math.random()*wordBank.length]
+var word = wordBank[Math.floor (Math.random()*wordBank.length)];
 
 // Display _ for each letter in the random word
+var answer = [];
+for (i=0; i < word.length; i++) {
+    answer[i] = "_";
+    console.log(answer);
+}
 
 // Check if input equals one of the letters
+
+
 
 // If input matches letter, replace _ with correct letter
 
