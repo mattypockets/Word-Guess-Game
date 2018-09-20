d169d78a517e5c38e507b298cc98c21fecd3aea2
Matt Gasper <mrgasper27@gmail.com>
2018-09-20 09:56:49 -0500

    Added bulk of JS. Organization, cleanup, and more functions needed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,28 +14,117 @@ var wordBank = ["mario",
 
 // Establish other base variables
 
-var wins = 0;
-var guessesLeft = 7;
+var wins = 0;               // Number of wins
+var guessesLeft = 7;        // Number of guesses left
+var lettersGuessed = [];    // Stores which letters have been guessed
+var gameOver = true;       // Flag to start/restart the game
+var guessingWordIndex;      // Index of the current word
+var guessingWord = [];      // Current word
 
-var letters = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
-
-// Choose a random word from wordBank
-var word = wordBank[Math.floor (Math.random()*wordBank.length)];
-
-// Display _ for each letter in the random word
-var answer = [];
-for (i=0; i < word.length; i++) {
-    answer[i] = "_";
-    console.log(answer);
-}
 
 // Check if input equals one of the letters
-
-
-
-// If input matches letter, replace _ with correct letter
-
 // If input does not match letter, reduce number of guesses by one
+function checkGuess(letter) {
+    // Array to store positions of letters in string
+    var positions = [];
+
+    // Loop through word finding all instances of guessed letter, store the indicies in an array.
+    for (var i = 0; i < guessingWord.length; i++) {
+        if(guessingWord[i] === letter) {
+            positions.push(i);
+        }
+    }
+
+    // if there are no indicies, remove a guess
+    if (positions.length < 0) {
+        guessesLeft--;
+    } else {
+        // Loop through all the indicies and replace the '_' with a letter.
+        for(var i = 0; i < positions.length; i++) {
+            guessingWord[positions[i]] == letter;
+        }
+    }
+};
+
+
 
 // List which letters have been guessed
 
+
+
+
+
+// Reset Game
+function resetGame() {
+
+    guessesLeft = 7;
+
+    // Choose a random word from wordBank
+    guessingWordIndex = Math.floor(Math.random()*wordBank.length);
+
+    lettersGuessed = [];
+    guessingWord = [];
+
+    // Create Guessing Word for page
+    for (i=0; i<wordBank[guessingWordIndex].length; i++) {
+        guessingWord.push("_");
+    }
+
+    updatePage();
+};
+
+
+
+
+
+// Update HTML on page
+function updatePage() {
+
+    document.getElementById("winCount").innerText = wins;
+
+    var wordDisplay = "";
+    for (var i = 0; i < guessingWord.length; i++) {
+        wordDisplay += guessingWord[i];
+    }
+
+    document.getElementById("currentWord").innerText = wordDisplay;
+    document.getElementById("guessesLeft").innerText = guessesLeft;
+    document.getElementById("lettersGuessed").innerText = lettersGuessed;
+};
+
+
+
+
+
+// Make a guess
+function makeGuess(letter) {
+    if (guessesLeft > 0) {
+        // Check if letter has been used yet
+        if (lettersGuessed.indexOf(letter) === -1) {
+            lettersGuessed.push(letter);
+            checkGuess(letter);
+        }
+    }
+};
+
+
+
+
+
+// Event listener
+document.onkeydown = function(event) {
+    // If we finished a game, dump one keystroke and reset.
+    if(gameOver) {
+        resetGame();
+        gameOver = false;
+    } 
+    else {
+        // Check to make sure a-z was pressed.
+        if(event.keyCode >= 65 && event.keyCode <= 90) {
+            makeGuess(event.key);
+            updatePage();
+            // checkWin();
+            // checkLoss();
+        }
+    }
+};
